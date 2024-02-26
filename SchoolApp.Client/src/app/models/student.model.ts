81daf14521aee19c3ed04ac1a1323d259bb945ed
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-02-26 08:36:00 +0300

    first commit

---
+++ SchoolApp.Client/src/app/models/student.model.ts
@@ -0,0 +1,10 @@
+export class StudentModel{
+    id: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    fullName: string  ="";
+    identityNumber: string = "";
+    studentNumber:number = 0;
+    classRoomId: string = "0";
+    index: number = 0;
+}
---
+++ SchoolApp.Server/NTierArchitecture.Business/Services/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using EntityFrameworkCorePagination.Nuget.Pagination;
+using NTierArchitecture.Entities.DTOs;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.Business;
+
+public interface IStudentService
+{
+    string Create(CreateStudentDto request);
+    string Update(UpdateStudentDto request);
+    string DeleteById(Guid id);
+    List<Student> GetAll();
+    Task<PaginationResult<Student>> GetAllByClassRoomIdAsync(PaginationRequestDto request);
+}
