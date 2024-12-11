f7f2603f4e12af940579b2b3b7109ecb65c77126
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-12-11 18:51:11 +0300

    Update student.model.ts

--- SchoolApp.Client/src/app/models/student.model.ts
+++ SchoolApp.Client/src/app/models/student.model.ts
@@ -1,18 +1,11 @@
 export class StudentModel{
     id: string = "";
-
     firstName: string = "";
-
     lastName: string = "";
-
     fullName: string  ="";
-
     identityNumber: string = "";
-
     studentNumber:number = 0;
-
     classRoomId: string = "0";
-
     index: number = 0;
     
 }
