2c991add480827b58d6fd282157ca8f83e6f7792
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-12-25 23:46:42 +0300

    Backend kısmı düzenlendi.

---
+++ SchoolApp.Server/SchoolApp.Business/Services/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using SchoolApp.Entities.DTOs;
+using SchoolApp.Entities.Models;
+
+namespace SchoolApp.Business.Services;
+public interface IStudentService
+{
+    string Create(CreateStudentDto request);
+    string Update(UpdateStudentDto request);
+    string DeleteById(Guid id);
+    List<Student> GetAll();
+}
---
+++ SchoolApp.Server/SchoolApp.WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolApp.Business.Services;
+using SchoolApp.Entities.DTOs;
+
+namespace SchoolApp.WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class StudentsController
+    (IStudentService studentService) : ControllerBase
+{
+    [HttpPost]
+
+    public IActionResult Create(CreateStudentDto request)
+    {
+        string message = studentService.Create(request);
+        return Ok(new { Message = message });
+    }
+}
