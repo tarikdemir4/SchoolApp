0d842971227294a8ba61dba73159178cc9860a78
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-12-26 14:08:31 +0300

    .

--- SchoolApp.Server/SchoolApp.Entities/DTOs/UpdateStudentDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolApp.Entities.DTOs;
-
-public sealed record UpdateStudentDto(
-    Guid Id,
-    string FirstName,
-    string LastName,
-    int StudentNumber,
-    string IdentityNumber,
-    Guid ClassRoomId);
