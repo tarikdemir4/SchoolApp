b7ed058a84d993967743b3b0e0d10b5b19b528a5
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2025-01-12 00:03:45 +0300

    Update app.component.ts

--- SchoolApp.Client/src/app/app.component.ts
+++ SchoolApp.Client/src/app/app.component.ts
@@ -7,8 +7,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  
   imports: [RouterOutlet],
+  
   template: "<router-outlet></router-outlet>"
 })
 export class AppComponent {}
