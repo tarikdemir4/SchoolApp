7e9ac9313eeee8e4f316b75920dda9434adfa193
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2025-02-06 23:38:38 +0300

    Update app.component.ts

--- SchoolApp.Client/src/app/app.component.ts
+++ SchoolApp.Client/src/app/app.component.ts
@@ -3,14 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 
 
-@Component({  selector: 'app-root',
-
-
-
-
-
-
-  
+@Component({  selector: 'app-root',  
   standalone: true,
 
   imports: [RouterOutlet],
